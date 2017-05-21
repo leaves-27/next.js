45f89f7f3a3438b8d1bae9508af92ad1bc5a9e14
dong.keke <dong.keke@tongbanjie.com>
2017-05-21 16:28:04 +0800

    11

--- components/message.js
+++ components/message.js
@@ -3,7 +3,8 @@ const Messsage = () => (
     <p style={{ 
         "padding-left":"300px",
         "margin-top":"100px",
-        "font-size":"20px"
+        "font-size":"20px",
+        "margin-bottom":"100px"
     }}>
       正在紧急开发中，敬请期待。
     </p>
