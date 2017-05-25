5c781dc6f4102360627e8f88b235ae2b4b1fcb5f
dong.keke <dong.keke@tongbanjie.com>
2017-05-25 23:54:11 +0800

    修改联系方式

--- components/header.js
+++ components/header.js
@@ -8,9 +8,9 @@ const Header = () => (
       </div>
       <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul className="nav navbar-nav navbar-right">
-          <li><span style={{ "color":"#999"}}>本书译自Learning Next.js英文版.</span>如需英文版，请访问<a style={{ "display":"inline-block","padding":"15px 2px","color":"#2196F3" }} href="https://learnnextjs.com/basics/getting-started" target="_blank">英文版</a></li>
+          <li><span style={{ "color":"#999"}}>本书译自Learning Next.js英文版，如需英文版，请访问</span><a style={{ "display":"inline-block","padding":"15px 2px","color":"#2196F3" }} href="https://learnnextjs.com/basics/getting-started" target="_blank">英文版</a></li>
           <li><span style={{ "marginTop":"10px","marginLeft":"10px" }} type="button" className="btn btn-danger btn-sm" data-toggle="modal" data-target="#modal">打赏</span></li>
-          <li><a href="javascript:void(0)">本站作者联系方式:1132761957@qq.com</a></li>
+          <li><a href="javascript:void(0)">QQ邮箱:1132761957@qq.com</a></li>
         </ul>
       </div>
     </div>
