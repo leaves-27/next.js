ddcc92129804c635bdeafc8c4ee306cecc2bcbfb
dong.keke <dong.keke@tongbanjie.com>
2017-05-26 00:10:54 +0800

    修改联系方式

--- components/header.js
+++ components/header.js
@@ -10,7 +10,7 @@ const Header = () => (
         <ul className="nav navbar-nav navbar-right">
           <li><span style={{ "color":"#999"}}>本书译自Learning Next.js英文版，如需英文版，请访问</span><a style={{ "display":"inline-block","padding":"15px 2px","color":"#2196F3" }} href="https://learnnextjs.com/basics/getting-started" target="_blank">英文版</a></li>
           <li><span style={{ "marginTop":"10px","marginLeft":"10px" }} type="button" className="btn btn-danger btn-sm" data-toggle="modal" data-target="#modal">打赏</span></li>
-          <li><a href="javascript:void(0)">QQ邮箱:1132761957@qq.com</a></li>
+          <li><a href="javascript:void(0)">QQ邮箱:1715302456@qq.com</a></li>
         </ul>
       </div>
     </div>
